kokonect-link/cherrypick

fix(backend): 움직이는 이미지를 사용한 사용자의 아바타 장식이 원격 서버에서 정지된 이미지로 보일 수 있음
test

packages/backend/src/core/AvatarDecorationService.ts
@@ -180,7 +180,7 @@ export class AvatarDecorationService implements OnApplicationShutdown {
 			const decorationData = {
 				name: name,
 				description: description,
-				url: this.getProxiedUrl(avatarDecoration.url, 'static'),
+				url: this.getProxiedUrl(avatarDecoration.url, 'avatar'),
 				remoteId: avatarDecorationId,
 				host: userHost,
 			};

sharlayan/misskey

packages/backend/src/core/AvatarDecorationService.ts
@@ -181,7 +181,7 @@ export class AvatarDecorationService implements OnApplicationShutdown {
 			const decorationData = {
 				name: name,
 				description: description,
-				url: this.getProxiedUrl(userAvatarDecoration.url),
+				url: this.getProxiedUrl(userAvatarDecoration.url, 'avatar'),
 				remoteId: userAvatarDecorationId,
 				host: userHost,
 			};